312622c1a4a4733fc6a66255f7bcf8b9bac308c5
Polozov2005 <aleksandr.polozov.2005@gmail.com>
2024-05-28 14:26:48 +0700

    Update interface.py

--- common/interface.py
+++ common/interface.py
@@ -28,7 +28,7 @@ conditions_frame.grid(
     row=0, column=1, padx=10, pady=(20, 10), sticky="nsew"
 )
 for index in range(5):
-    conditions_frame.columnconfigure(index=index, weight=1)
+    conditions_frame.rowconfigure(index=index, weight=1)
 
 
 solve_frame = ttk.LabelFrame(root, text="Решить", padding=(20, 10))
@@ -43,22 +43,68 @@ answer_frame.grid(
 
 
 
-u_frame = ttk.Frame(conditions_frame)
-u_frame.grid(
+equation_u_frame = ttk.Frame(conditions_frame)
+equation_u_frame.grid(
     row=0, column=0, padx=10, pady=(20, 10), sticky="nsew"
 )
-u_label = ttk.Label(u_frame, text ="u(t)=sincos schkibididopeeyes")
-u_label.grid(
+equation_u_label = ttk.Label(equation_u_frame, text ="u(t) = sincos schkibididopeeyes")
+equation_u_label.grid(
     row=0, column=0, sticky='nsew'
 )
 
+equation_U_frame = ttk.Frame(conditions_frame)
+equation_U_frame.grid(
+    row=1, column=0, padx=10, pady=(20, 10), sticky='nsew'
+)
+equation_U_label = ttk.Label(equation_U_frame, text ="U = integral(U_0foisdjfsjdfl)")
+equation_U_label.grid(
+    row=0, column=0, sticky='nsew'
+)
+
+f_frame = ttk.Frame(conditions_frame)
+f_frame.grid(
+    row=2, column=0, padx=10, pady=(20, 10), sticky='nsew'
+)
+f_label = ttk.Label(f_frame, text="f = ")
+f_label.grid(
+    row=0, column=0, sticky='nsew'
+)
+f_entry = ttk.Entry(f_frame)
+f_entry.grid(
+    row=0, column=1, sticky='nsew'
+)
+f_unit_label = ttk.Label(f_frame, text="Гц")
+f_unit_label.grid(
+    row=0, column=2, sticky='nsew'
+)
 
 U_frame = ttk.Frame(conditions_frame)
 U_frame.grid(
-    row=1, column=0, padx=10, pady=(20, 10), sticky='nsew'
+    row=3, column=0, padx=10, pady=(20, 10), sticky='nsew'
 )
-U_label = ttk.Label(U_frame, text ="U = integral(U_0foisdjfsjdfl)")
+U_label = ttk.Label(U_frame, text="U = ")
 U_label.grid(
+    row=0, column=0, sticky='nsew'
+)
+U_entry = ttk.Entry(U_frame)
+U_entry.grid(
+    row=0, column=1, sticky='nsew'
+)
+U_unit_label = ttk.Label(U_frame, text="В")
+U_unit_label.grid(
+    row=0, column=2, sticky='nsew'
+)
+
+optionmenu_frame = ttk.Frame(conditions_frame)
+optionmenu_frame.grid(
+    row=4, column=0, padx=10, pady=(20, 10), sticky='nsew'
+)
+optionmenu_list = ["", "Решение встроенной функцией", "Решение реализованной функцией"]
+optionmenu_var = tk.StringVar(value=optionmenu_list[1])
+optionmenu = ttk.OptionMenu(
+    optionmenu_frame, optionmenu_var, *optionmenu_list
+)
+optionmenu.grid(
     row=0, column=0, sticky='nsew'
 )
 
