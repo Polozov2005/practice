ead8f75332a6ec5f3699df00f240eb66f5824445
Polozov2005 <aleksandr.polozov.2005@gmail.com>
2024-05-29 09:58:48 +0700

    Update interface.py

--- common/interface.py
+++ common/interface.py
@@ -13,14 +13,14 @@ root.resizable(False, False)
 root.tk.call("source", "common/azure.tcl")
 root.tk.call("set_theme", "dark")
 
-for index in [0, 1]:
-    root.columnconfigure(index=index, weight=1)
-for index in [0, 1, 2]:
+root.columnconfigure(index=0, weight=2)
+root.columnconfigure(index=1, weight=1)
+for index in [0, 2]:
     root.rowconfigure(index=index, weight=1)
 
 graph_frame = ttk.LabelFrame(root, text="График функции u(t)", padding=(20, 10))
 graph_frame.grid(
-    row=0, column=0, padx=10, pady=(20, 10), sticky="nsew", rowspan=3
+    row=0, column=0, sticky="nsew", rowspan=3
 )
 
 conditions_frame = ttk.LabelFrame(root, text="Начальные условия", padding=(20, 10))
@@ -30,16 +30,18 @@ conditions_frame.grid(
 for index in range(5):
     conditions_frame.rowconfigure(index=index, weight=1)
 
-
-solve_frame = ttk.LabelFrame(root, text="Решить", padding=(20, 10))
+# solve_frame = ttk.LabelFrame(root, text="Решить", padding=(20, 10))
+solve_frame = ttk.Frame(root, padding=(20, 10))
 solve_frame.grid(
-    row=1, column=1, padx=10, pady=(20, 10), sticky="nsew"
+    row=1, column=1, padx=10, pady=(20, 0), sticky="nsew"
 )
 
 answer_frame = ttk.LabelFrame(root, text="Ответ", padding=(20, 10))
 answer_frame.grid(
     row=2, column=1, padx=10, pady=(20, 10), sticky="nsew"
 )
+for index in range(2):
+    answer_frame.rowconfigure(index=index, weight=1)
 
 equation_u_frame = ttk.Frame(conditions_frame)
 equation_u_frame.grid(
@@ -102,9 +104,57 @@ optionmenu_var = tk.StringVar(value=optionmenu_list[1])
 optionmenu = ttk.OptionMenu(
     optionmenu_frame, optionmenu_var, *optionmenu_list
 )
+optionmenu.config(width = 33)
 optionmenu.grid(
     row=0, column=0, sticky='nsew'
 )
+
+
+solve_button = ttk.Button(solve_frame, text="Решить", style="Accent.TButton")
+solve_button.config(width=30)
+solve_button.grid(row=0, column=0, padx=10, sticky="nsew")
+# solve_button.place(x=, y=0)
+
+
+
+
+
+T_frame = ttk.Frame(answer_frame)
+T_frame.grid(
+    row=0, column=0, padx=10, pady=(20, 10), sticky='nsew'
+)
+T_label = ttk.Label(T_frame, text="T = ")
+T_label.grid(
+    row=0, column=0, sticky='nsew'
+)
+T_entry = ttk.Entry(T_frame, state="readonly")
+T_entry.grid(
+    row=0, column=1, sticky='nsew'
+)
+T_unit_label = ttk.Label(T_frame, text="с")
+T_unit_label.grid(
+    row=0, column=2, sticky='nsew'
+)
+
+U_0_frame = ttk.Frame(answer_frame)
+U_0_frame.grid(
+    row=1, column=0, padx=10, pady=(20, 10), sticky='nsew'
+)
+U_0_label = ttk.Label(U_0_frame, text="U_0 = ")
+U_0_label.grid(
+    row=0, column=0, sticky='nsew'
+)
+U_0_entry = ttk.Entry(U_0_frame, state="readonly")
+U_0_entry.grid(
+    row=0, column=1, sticky='nsew'
+)
+U_0_unit_label = ttk.Label(U_0_frame, text="В")
+U_0_unit_label.grid(
+    row=0, column=2, sticky='nsew'
+)
+
+
+
 
 root.update()
 x_cordinate = int((root.winfo_screenwidth() / 2) - (root.winfo_width() / 2))
